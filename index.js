05112a37be45a9111e37f7bb04acc2db80a55b13
𝓥𝓮𝓮𝓻𝓪 <veeraprasad.koduri@gmail.com>
2026-09-06 20:27:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res)=>{
-    return res.json({msg : "Hello Frome server v2 with Love by kashish"})
+    return res.json({msg : "Hello Frome server v2 with Love by veeraprasadportfolio.online"})
 })
 
 app.listen(PORT,()=>{
